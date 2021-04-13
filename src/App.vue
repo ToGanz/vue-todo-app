6194e237faffcd28c996422c87bc5f5376c1d849
tganz <tganz5890@googlemail.com>
2021-04-13 14:19:21 +0200

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1><span>Just do it</span></h1>
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    data() {
+      projects: []
+    },
+    methods: {
+      addProject(title) {
+        const project = {
+          id: new Date().toISOString(),
+          title,
+          tasks = []
+        }
+        this.projects.push(project);
+      },
+      deleteProject(projectId) {
+        this.projects = this.projects.filter(
+        (project) => project.id !== projectId
+        );
+      }
+
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
